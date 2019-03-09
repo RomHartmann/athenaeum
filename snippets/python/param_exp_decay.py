18cb612497408fe28b198d7340a11f76d2313eea
RomHartmann <romhartmann@gmail.com>
2019-03-08 17:27:01 -0800

    tweak the query

---
+++ snippets/python/param_exp_decay.py
@@ -0,0 +1,68 @@
+import from collections import namedtuple
+
+import numpy as np
+
+
+def exp_decay(x0=0, y0=1, x1=200, y1=0.5):
+    """Calculate the coefficients of an exponential decay function.
+
+    :param x0:
+    :type x0:
+    :param y0:
+    :type y0:
+    :param x1:
+    :type x1:
+    :param y1:
+    :type y1:
+    :return:
+    :rtype:
+    """
+
+def recip_params(x_max, y_max, y_min=1, x_left_shift=1):
+    """Calculate the parameters for the Solr recip function.
+
+    Based on min and max boost value and the corresponding maximum boost
+      amount.
+    The maximum value boost amount, y_max, occurs at x = 0.  Assigned x0, y0
+        This boost value causes no change to the assigned value
+    The minimum boost value, y_min, orrurs at x_max.  Assigned x0, y1
+        This function is assymptotical, so a "close enough" constant is chosen
+    All x and y values are larger than zero (in which case negative values,
+      like age are considered as "how long ago in positive numbers")
+
+    y = a / (mx + b)
+
+    y0 = y_min = a / b   # x0 == 0
+        a = y_min * b
+
+    y1 = assymp_thresh = a / (m * x_max + b)
+
+    a = (m * x_max) + b   # y_max == 1
+
+    :param x_max: x1 at which minimum boost occurs
+    :type x_max: float or int
+    :param y_max: The max-boost multiplyer at x0
+    :type y_max: float or int
+    :param y_min: Assymptotical approximation min boost at x1
+    :type y_min: float or int
+    :param x_left_shift: b-variable that is responsible for y-intercept at x=0
+        Shifts the whole graph to the left.  Should not == 0 else inf at x=0
+    :type x_left_shift: float or int
+    :return: (m, a, b) to parameterize the solr recip function
+    :rtype: collections.namedtuple
+    """
+    y0 = float(y_max)
+    y1 = float(y_min)
+    x1 = float(x_max)
+    b = float(x_left_shift)
+
+    # From x_min == 0 and y_max:
+    a = y0 * b
+
+    # From x_max and and y_min
+    m = (((y0 * b) / y1) - b)/x1
+
+    params = namedtuple('Params', 'm a b')
+    return params(m, a, b)
+# find these and then remove scoop and haram
+
---
+++ snippets/python/point_to_segment_dist.py
@@ -0,0 +1,59 @@
+"""calculates the nearest distance from a point to a line segment."""
+import numpy as np
+
+
+def point_to_line_dist(point, line):
+    """Calculate the distance between a point and a line segment.
+
+    To calculate the closest distance to a line segment, we first need to check
+    if the point projects onto the line segment.  If it does, then we calculate
+    the orthogonal distance from the point to the line.
+    If the point does not project to the line segment, we calculate the
+    distance to both endpoints and take the shortest distance.
+
+    :param point: Numpy array of form [x,y], describing the point.
+    :type point: numpy.core.multiarray.ndarray
+    :param line: list of endpoint arrays of form [P1, P2]
+    :type line: list of numpy.core.multiarray.ndarray
+    :return: The minimum distance to a point.
+    :rtype: dict
+    """
+    # unit vector
+    unit_line = line[1] - line[0]
+    norm_unit_line = unit_line / np.linalg.norm(unit_line)
+
+    # compute the perpendicular distance to the theoretical infinite line
+    segment_dist = (
+        np.linalg.norm(np.cross(line[1] - line[0], line[0] - point)) /
+        np.linalg.norm(unit_line)
+    )
+
+    diff = (
+        (norm_unit_line[0] * (point[0] - line[0][0])) +
+        (norm_unit_line[1] * (point[1] - line[0][1]))
+    )
+
+    x_seg = (norm_unit_line[0] * diff) + line[0][0]
+    y_seg = (norm_unit_line[1] * diff) + line[0][1]
+
+    # decide if the intersection point falls on the line segment
+    lp1_x = line[0][0]  # line point 1 x
+    lp1_y = line[0][1]  # line point 1 y
+    lp2_x = line[1][0]  # line point 2 x
+    lp2_y = line[1][1]  # line point 2 y
+    is_betw_x = lp1_x <= x_seg <= lp2_x or lp2_x <= x_seg <= lp1_x
+    is_betw_y = lp1_y <= y_seg <= lp2_y or lp2_y <= y_seg <= lp1_y
+
+    if is_betw_x and is_betw_y:
+        return {
+            "dist_to_line": segment_dist,
+            "nearest_point_on_line": np.array([x_seg, y_seg])
+        }
+    else:
+        # if not, then return the minimum distance to the segment endpoints
+        nearest_endpoint = line[0] if np.linalg.norm(line[0] - point) <= np.linalg.norm(line[1] - point) else line[1]
+        endpoint_dist = np.linalg.norm(nearest_endpoint - point)
+        return {
+            "dist_to_line": endpoint_dist,
+            "nearest_point_on_line": nearest_endpoint
+        }
